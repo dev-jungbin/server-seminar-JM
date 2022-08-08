b202c0c94a2662d9db3e60c193924321040f8a8a
dev-madilyn <lollin0@naver.com>
2022-08-09 02:44:11 +0900

    Practice: variable

---
+++ seminar1/variable.js
@@ -0,0 +1,29 @@
+var variable1 = "var variable";
+var variable1 = "new var variable";
+
+console.log("var: ", variable1);
+
+// let variable2 = "let variable"; // 얘 상수임 재선언 ㄴㄴ
+// let variable2 = "new let variable";
+
+// console.log("let: ", variable2);
+
+// const variable3 = "const variable"; // 얘 상수자나 재선언 ㄴㄴ
+// const variable3 = "new const variable";
+
+// console.log("const: ", variable3)
+
+var variable4 = "var variable";
+variable4 = "melong babo";
+
+console.log("variable4: ", variable4);
+
+let variable5 = "let variable";
+variable5 = "melong hi";
+
+console.log("variable5: ", variable5);
+
+const variable6 = "const variable";
+variable6 = "melong music..";
+
+console.log("variable6: ", variable6);
